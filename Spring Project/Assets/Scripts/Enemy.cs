8ade04fa069846e6febb207f581f071483325a76
bikerLee47 <lr00891@srcs.org>
2020-05-27 23:34:25 -0700

    Prototype 1

---
+++ Spring Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject gameObject;
+    private Transform target;
+    private float speed = 0.07f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        transform.LookAt(target.transform);
+        transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime / speed);
+    }
+}
